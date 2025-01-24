1f777e626d930e8f8aaf0b9c5ffe98174093f6df
Esakkipandi0214 <praveenpandian01@gmail.com>
2025-01-24 22:43:23 +0530

    updates

--- src/components/CoursesComponents/CourseStats.tsx
+++ src/components/CoursesComponents/CourseStats.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const courseStats = () => {
+const CourseStats = () => {
   return (
-    <div className=' bg-transparent border rounded-lg border-gray-400 text-black shadow-2xl'>Course stats</div>
+    <div className=' bg-transparent border rounded-lg border-gray-400 text-black shadow-2xl'>
+        <div>
+            <h1 className=' text-black'>Course Name</h1>
+            <p>Python is a versatile, high-level programming language known for its simplicity and readability, making it ideal for beginners and professionals alike.</p>
+        </div>
+        <div></div>
+    </div>
   )
 }
 
-export default courseStats
+export default CourseStats
